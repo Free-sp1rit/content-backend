c76fbd4b3979c1a805e9b9f5f2172025bb3f384f
Free-sp1rit <soteyimg@outlook.com>
2026-03-20 19:36:39 +0800

    feat(server): bootstrap minimal http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"content-backend/internal/handler"
+	"content-backend/internal/repository"
+	"content-backend/internal/service"
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("content backend starting")
+	db, err := sql.Open(
+		"postgres",
+		"host=localhost port=5432 user=postgres dbname=content_backend sslmode=disable",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userRepo := repository.NewUserRepository(db)
+	articleRepo := repository.NewArticleRepository(db)
+
+	authService := service.NewAuthService(userRepo)
+	articleService := service.NewArticleService(articleRepo)
+
+	authHandler := handler.NewAuthHandler(authService)
+	_ = articleService
+
+	http.HandleFunc("/register", authHandler.Register)
+	http.HandleFunc("/login", authHandler.Login)
+
+	log.Println("server listening on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
