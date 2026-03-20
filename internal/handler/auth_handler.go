6bef1c91e916fe3ff624b55dc1dcd65b40ccac95
Free-sp1rit <soteyimg@outlook.com>
2026-03-20 16:39:43 +0800

    feat(handler): implement register and login

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -1,1 +1,99 @@
 package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"content-backend/internal/service"
+)
+
+type AuthHandler struct {
+	authService *service.AuthService
+}
+
+func NewAuthHandler(authService *service.AuthService) *AuthHandler {
+	return &AuthHandler{authService: authService}
+}
+
+type RegisterRequest struct {
+	Email		 string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RegisterResponse struct {
+	ID int64 `json:"id"`
+}
+
+func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req RegisterRequest
+	err :=  json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id, err := h.authService.Register(r.Context(), req.Email, req.Password)
+	if err != nil {
+		if errors.Is(err, service.ErrEmailAlreadyRegistered) {
+			w.WriteHeader(http.StatusConflict)
+			return
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res := RegisterResponse{ID: id}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(res)
+
+	return
+}
+
+type LoginRequest struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	ID int64 `json:"id"`
+}
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	if(r.Method != http.MethodPost) {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id, err := h.authService.Login(r.Context(), req.Email, req.Password)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidCredentials) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res := LoginResponse{ID: id}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(res)
+
+	return
+}
